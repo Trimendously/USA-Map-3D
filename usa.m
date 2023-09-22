90f3cbc33f3f9e1f42cdc43ee6b4eec99a97e005
Noah Trim <55414467+Trimendously@users.noreply.github.com>
2023-09-22 19:30:17 -0400

    Updated color
    -Created a custom color map
    -Made it into a dark theme

--- usa.m
+++ usa.m
@@ -18,7 +18,7 @@ ax1 = subplot(1, 2, 1);  % Left section for the plot
 ax2 = subplot(1, 2, 2);  % Right section for the legend
 
 % Plots the states in the left section
-plotBackground = [1, 1, 1]; % RGB values
+plotBackground = [0, 0, 0]; % RGB values
 axes(ax1);
 ax1.Color = plotBackground;
 hold on;
@@ -29,8 +29,63 @@ states = shaperead('usastatelo', 'UseGeoCoords', true, 'BoundingBox', [-180, -90
 % Geolocation to cartesian
 [x,y] = lambert(states);
 
+
+distinct_colors = [
+    0.620, 0.125, 0.294;  % Alabama (Crimson)
+    0.086, 0.317, 0.482;  % Alaska (Dark Blue)
+    0.769, 0.102, 0.125;  % Arizona (Red)
+    0.090, 0.184, 0.373;  % Arkansas (Navy Blue)
+    0.839, 0.282, 0.098;  % California (Orange-Red)
+    0.533, 0.172, 0.227;  % Colorado (Burgundy)
+    0.137, 0.235, 0.423;  % Connecticut (Navy Blue)
+    0.902, 0.160, 0.235;  % Delaware (Red)
+    0.980, 0.349, 0.090;  % Florida (Orange)
+    0.612, 0.086, 0.141;  % Georgia (Red)
+    0.984, 0.502, 0.118;  % Hawaii (Orange)
+    0.278, 0.486, 0.169;  % Idaho (Green)
+    0.341, 0.360, 0.690;  % Illinois (Blue)
+    0.075, 0.306, 0.267;  % Indiana (Green)
+    0.502, 0.000, 0.149;  % Iowa (Dark Red)
+    0.906, 0.298, 0.235;  % Kansas (Red)
+    0.537, 0.404, 0.145;  % Kentucky (Olive)
+    0.541, 0.149, 0.196;  % Louisiana (Maroon)
+    0.282, 0.322, 0.619;  % Maine (Blue)
+    0.000, 0.361, 0.533;  % Maryland (Navy Blue)
+    0.482, 0.569, 0.094;  % Massachusetts (Green)
+    0.384, 0.431, 0.435;  % Michigan (Dark Gray)
+    0.514, 0.675, 0.757;  % Minnesota (Light Blue)
+    0.216, 0.247, 0.271;  % Mississippi (Dark Gray)
+    0.506, 0.149, 0.341;  % Missouri (Maroon)
+    0.965, 0.855, 0.157;  % Montana (Yellow)
+    0.078, 0.388, 0.333;  % Nebraska (Green)
+    0.086, 0.176, 0.431;  % Nevada (Dark Blue)
+    0.251, 0.541, 0.796;  % New Hampshire (Blue)
+    0.937, 0.227, 0.243;  % New Jersey (Red)
+    0.275, 0.506, 0.725;  % New Mexico (Blue)
+    0.078, 0.361, 0.529;  % New York (Navy Blue)
+    0.251, 0.404, 0.698;  % North Carolina (Blue)
+    0.000, 0.306, 0.561;  % North Dakota (Dark Blue)
+    0.812, 0.196, 0.208;  % Ohio (Red)
+    0.094, 0.373, 0.102;  % Oklahoma (Green)
+    0.749, 0.196, 0.384;  % Oregon (Red)
+    0.145, 0.251, 0.396;  % Pennsylvania (Dark Blue)
+    0.000, 0.271, 0.529;  % Rhode Island (Navy Blue)
+    0.141, 0.317, 0.470;  % South Carolina (Dark Blue)
+    0.086, 0.129, 0.431;  % South Dakota (Dark Blue)
+    0.620, 0.125, 0.294;  % Tennessee (Crimson)
+    0.000, 0.267, 0.486;  % Texas (Dark Blue)
+    0.000, 0.337, 0.620;  % Utah (Navy Blue)
+    0.086, 0.227, 0.435;  % Vermont (Dark Blue)
+    0.639, 0.086, 0.216;  % Virginia (Maroon)
+    0.043, 0.275, 0.557;  % Washington (Blue)
+    0.533, 0.102, 0.243;  % West Virginia (Maroon)
+    0.000, 0.286, 0.482;  % Wisconsin (Dark Blue)
+    0.239, 0.275, 0.435;  % Wyoming (Dark Blue)
+    0.737, 0.737, 0.737;  % Washington, D.C. (Light Gray)
+];
+
 % Creates a color map based on the number of states
-colorMap = colormap(colorcube(numel(states)));
+colorMap = colormap(distinct_colors);
 lighterMap = brighten(colorcube,0.7);
 colorMapLighter = colormap(lighterMap);
 
@@ -57,17 +112,16 @@ for i = 1:numel(states)
         displayName = [states(i).Name, ' (Count: ', num2str(stateCounts(states(i).Name)), ')'];
         % Added a plus 1 if we decide to keep the base map
         for j = 1:(stateCounts(states(i).Name)+1)
-            if j == 1
-                patch(ax1, x_coord, y_coord, j*z_coord, colorMap(i, :), 'DisplayName', displayName);  
-            else
-                patch(ax1, x_coord, y_coord, j*z_coord, colorMap(i, :), 'DisplayName', displayName);  
+            p = patch(ax1, x_coord, y_coord, j*z_coord, colorMap(i, :), 'DisplayName', displayName,'EdgeColor', 'w');  
+            if j ~= 1
+                alpha(p, 1);  % Sets transparency
             end
         end
     else
         %fprintf('- State Count: %d\n', 0);
         displayName = [states(i).Name, ' (Count: ', num2str(0), ')'];
         % If the base map is kept
-        patch(ax1, x_coord, y_coord, z_coord, colorMap(i, :), 'DisplayName', displayName);       
+        patch(ax1, x_coord, y_coord, z_coord, colorMap(i, :), 'DisplayName', displayName,'EdgeColor', 'w');       
     end
     
     % Store the legend label for this state
